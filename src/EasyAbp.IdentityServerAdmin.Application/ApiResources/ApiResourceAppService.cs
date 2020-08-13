67d8e2665e19c578a585d3969497e4b1623774d0
Kefeng Wei <kefengwei@live.com>
2020-08-13 16:53:23 +0800

    fix update exist check

--- src/EasyAbp.IdentityServerAdmin.Application/ApiResources/ApiResourceAppService.cs
+++ src/EasyAbp.IdentityServerAdmin.Application/ApiResources/ApiResourceAppService.cs
@@ -15,71 +15,71 @@ using System.Linq;
 
 namespace EasyAbp.IdentityServerAdmin.Application.EasyAbp.IdentityServerAdmin.ApiResources
 {
-    [Authorize(IdentityServerAdminPermissions.ApiResource.Default)]
-    public class ApiResourceAppService : ReadOnlyAppService<ApiResource, ApiResourceDto, Guid, GetApiResourceListInput>, IApiResourceAppService
+  [Authorize(IdentityServerAdminPermissions.ApiResource.Default)]
+  public class ApiResourceAppService : ReadOnlyAppService<ApiResource, ApiResourceDto, Guid, GetApiResourceListInput>, IApiResourceAppService
+  {
+    private readonly IApiResourceRepository _repository;
+
+    public ApiResourceAppService(IApiResourceRepository repository) : base((IReadOnlyRepository<ApiResource, Guid>)repository)
     {
-        private readonly IApiResourceRepository _repository;
-
-        public ApiResourceAppService(IApiResourceRepository repository) : base((IReadOnlyRepository<ApiResource, Guid>)repository)
-        {
-            _repository = repository;
-        }
-
-
-
-        public override async Task<PagedResultDto<ApiResourceDto>> GetListAsync(GetApiResourceListInput input)
-        {
-
-            var totalCount = await _repository.GetCountAsync();
-            var entities = await _repository.GetListAsync(input.Sorting, input.SkipCount, input.MaxResultCount, null, includeDetails: true);
-            return new PagedResultDto<ApiResourceDto>(totalCount, entities.Select(MapToGetListOutputDto).ToList());
-
-        }
-
-        public override async Task<ApiResourceDto> GetAsync(Guid id)
-        {
-            var apiResource = await GetEntityByIdAsync(id);
-            return MapToGetListOutputDto(apiResource);
-        }
-
-
-        [Authorize(IdentityServerAdminPermissions.ApiResource.Create)]
-        public async Task<ApiResourceDto> CreateAsync(CreateApiResourceInputDto input)
-        {
-
-            var nameExist = await _repository.CheckNameExistAsync(input.Name);
-
-            if (nameExist)
-            {
-                throw new ApiResourceNameNowAllowedDuplicateException(input.Name);
-            }
-
-            var apiResource = new ApiResource(GuidGenerator.Create(), input.Name, input.DisplayName, input.Description);
-            input.UserClaims.ForEach(x => apiResource.AddUserClaim(x));
-            apiResource = await _repository.InsertAsync(apiResource);
-            return MapToGetOutputDto(apiResource);
-        }
-
-        [Authorize(IdentityServerAdminPermissions.ApiResource.Update)]
-        public async Task<ApiResourceDto> UpdateAsync(Guid id, UpdateApiResourceInputDto input)
-        {
-            var nameExist = await _repository.CheckNameExistAsync(input.Name, id);
-
-            if (nameExist)
-            {
-                throw new ApiResourceNameNowAllowedDuplicateException(input.Name);
-            }
-            var apiResource = await _repository.FindAsync(id, true);
-            apiResource = ObjectMapper.Map<UpdateApiResourceInputDto, ApiResource>(input, apiResource);
-            apiResource = await _repository.UpdateAsync(apiResource);
-            return MapToGetListOutputDto(apiResource);
-        }
-
-        [Authorize(IdentityServerAdminPermissions.ApiResource.Delete)]
-        public async Task DeleteAsync(Guid id)
-        {
-            var apiResource = await _repository.FindAsync(id);
-            await _repository.DeleteAsync(apiResource);
-        }
+      _repository = repository;
     }
+
+
+
+    public override async Task<PagedResultDto<ApiResourceDto>> GetListAsync(GetApiResourceListInput input)
+    {
+
+      var totalCount = await _repository.GetCountAsync();
+      var entities = await _repository.GetListAsync(input.Sorting, input.SkipCount, input.MaxResultCount, null, includeDetails: true);
+      return new PagedResultDto<ApiResourceDto>(totalCount, entities.Select(MapToGetListOutputDto).ToList());
+
+    }
+
+    public override async Task<ApiResourceDto> GetAsync(Guid id)
+    {
+      var apiResource = await GetEntityByIdAsync(id);
+      return MapToGetListOutputDto(apiResource);
+    }
+
+
+    [Authorize(IdentityServerAdminPermissions.ApiResource.Create)]
+    public async Task<ApiResourceDto> CreateAsync(CreateApiResourceInputDto input)
+    {
+
+      var nameExist = await _repository.CheckNameExistAsync(input.Name);
+
+      if (nameExist)
+      {
+        throw new ApiResourceNameNowAllowedDuplicateException(input.Name);
+      }
+
+      var apiResource = new ApiResource(GuidGenerator.Create(), input.Name, input.DisplayName, input.Description);
+      input.UserClaims.ForEach(x => apiResource.AddUserClaim(x));
+      apiResource = await _repository.InsertAsync(apiResource);
+      return MapToGetOutputDto(apiResource);
+    }
+
+    [Authorize(IdentityServerAdminPermissions.ApiResource.Update)]
+    public async Task<ApiResourceDto> UpdateAsync(Guid id, UpdateApiResourceInputDto input)
+    {
+      var nameExist = await _repository.CheckNameExistAsync(input.Name, id);
+
+      if (nameExist)
+      {
+        throw new ApiResourceNameNowAllowedDuplicateException(input.Name);
+      }
+      var apiResource = await _repository.FindAsync(id, true);
+      apiResource = ObjectMapper.Map<UpdateApiResourceInputDto, ApiResource>(input, apiResource);
+      apiResource = await _repository.UpdateAsync(apiResource);
+      return MapToGetListOutputDto(apiResource);
+    }
+
+    [Authorize(IdentityServerAdminPermissions.ApiResource.Delete)]
+    public async Task DeleteAsync(Guid id)
+    {
+      var apiResource = await _repository.FindAsync(id);
+      await _repository.DeleteAsync(apiResource);
+    }
+  }
 }
--- src/EasyAbp.IdentityServerAdmin.Application/IdentityResources/IdentityResourceAppService.cs
+++ src/EasyAbp.IdentityServerAdmin.Application/IdentityResources/IdentityResourceAppService.cs
@@ -12,65 +12,65 @@ using Volo.Abp.IdentityServer.IdentityResources;
 
 namespace EasyAbp.IdentityServerAdmin.IdentityResources
 {
-    [Authorize(IdentityServerAdminPermissions.IdentityResource.Default)]
-    public class IdentityResourceAppService : ReadOnlyAppService<IdentityResource, IdentityResourceDto, Guid, GetIdentityResourceListInputDto>, IIdentityResourceAppService
+  [Authorize(IdentityServerAdminPermissions.IdentityResource.Default)]
+  public class IdentityResourceAppService : ReadOnlyAppService<IdentityResource, IdentityResourceDto, Guid, GetIdentityResourceListInputDto>, IIdentityResourceAppService
+  {
+    private readonly IIdentityResourceRepository _repository;
+
+    public IdentityResourceAppService(IIdentityResourceRepository repository) : base((IReadOnlyRepository<IdentityResource, Guid>)repository)
     {
-        private readonly IIdentityResourceRepository _repository;
-
-        public IdentityResourceAppService(IIdentityResourceRepository repository) : base((IReadOnlyRepository<IdentityResource, Guid>)repository)
-        {
-            _repository = repository;
-        }
-        public override Task<PagedResultDto<IdentityResourceDto>> GetListAsync(GetIdentityResourceListInputDto input)
-        {
-            return base.GetListAsync(input);
-        }
-        public override Task<IdentityResourceDto> GetAsync(Guid id)
-        {
-            return base.GetAsync(id);
-        }
-
-        [Authorize(IdentityServerAdminPermissions.IdentityResource.Create)]
-        public async Task<IdentityResourceDto> CreateAsync(CreateIdentityResourceInputDto input)
-        {
-            var nameExist = await _repository.CheckNameExistAsync(input.Name.Trim());
-
-            if (nameExist)
-            {
-                throw new IdentityResourceNameNotAllowedDuplicateException(input.Name);
-            }
-            var identityResource = new IdentityResource(GuidGenerator.Create(), input.Name.Trim())
-            {
-                DisplayName = input.DisplayName?.Trim(),
-                Description = input.Description?.Trim(),
-                Enabled = input.Enabled,
-                Required = input.Required,
-                Emphasize = input.Emphasize,
-                ShowInDiscoveryDocument = input.ShowInDiscoveryDocument
-            };
-            input.UserClaims.ForEach(x => identityResource.AddUserClaim(x));
-            identityResource = await _repository.InsertAsync(identityResource);
-            return MapToGetOutputDto(identityResource);
-        }
-        [Authorize(IdentityServerAdminPermissions.IdentityResource.Update)]
-        public async Task<IdentityResourceDto> UpdateAsync(Guid id, UpdateIdentityResourceInputDto input)
-        {
-            var nameExist = await _repository.CheckNameExistAsync(input.Name);
-
-            if (nameExist)
-            {
-                throw new IdentityResourceNameNotAllowedDuplicateException(input.Name);
-            }
-            var identityResource = await _repository.FindAsync(id);
-            identityResource = ObjectMapper.Map<UpdateIdentityResourceInputDto, IdentityResource>(input, identityResource);
-            identityResource = await _repository.UpdateAsync(identityResource);
-            return MapToGetOutputDto(identityResource);
-        }
-        [Authorize(IdentityServerAdminPermissions.IdentityResource.Delete)]
-        public async Task DeleteAsync(Guid id)
-        {
-            var identityResource = await _repository.FindAsync(id, true);
-            await _repository.DeleteAsync(identityResource);
-        }
+      _repository = repository;
     }
+    public override Task<PagedResultDto<IdentityResourceDto>> GetListAsync(GetIdentityResourceListInputDto input)
+    {
+      return base.GetListAsync(input);
+    }
+    public override Task<IdentityResourceDto> GetAsync(Guid id)
+    {
+      return base.GetAsync(id);
+    }
+
+    [Authorize(IdentityServerAdminPermissions.IdentityResource.Create)]
+    public async Task<IdentityResourceDto> CreateAsync(CreateIdentityResourceInputDto input)
+    {
+      var nameExist = await _repository.CheckNameExistAsync(input.Name.Trim());
+
+      if (nameExist)
+      {
+        throw new IdentityResourceNameNotAllowedDuplicateException(input.Name);
+      }
+      var identityResource = new IdentityResource(GuidGenerator.Create(), input.Name.Trim())
+      {
+        DisplayName = input.DisplayName?.Trim(),
+        Description = input.Description?.Trim(),
+        Enabled = input.Enabled,
+        Required = input.Required,
+        Emphasize = input.Emphasize,
+        ShowInDiscoveryDocument = input.ShowInDiscoveryDocument
+      };
+      input.UserClaims.ForEach(x => identityResource.AddUserClaim(x));
+      identityResource = await _repository.InsertAsync(identityResource);
+      return MapToGetOutputDto(identityResource);
+    }
+    [Authorize(IdentityServerAdminPermissions.IdentityResource.Update)]
+    public async Task<IdentityResourceDto> UpdateAsync(Guid id, UpdateIdentityResourceInputDto input)
+    {
+      var nameExist = await _repository.CheckNameExistAsync(input.Name, id);
+
+      if (nameExist)
+      {
+        throw new IdentityResourceNameNotAllowedDuplicateException(input.Name);
+      }
+      var identityResource = await _repository.FindAsync(id);
+      identityResource = ObjectMapper.Map<UpdateIdentityResourceInputDto, IdentityResource>(input, identityResource);
+      identityResource = await _repository.UpdateAsync(identityResource);
+      return MapToGetOutputDto(identityResource);
+    }
+    [Authorize(IdentityServerAdminPermissions.IdentityResource.Delete)]
+    public async Task DeleteAsync(Guid id)
+    {
+      var identityResource = await _repository.FindAsync(id, true);
+      await _repository.DeleteAsync(identityResource);
+    }
+  }
 }
